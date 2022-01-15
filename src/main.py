2f36adb80e1faf2c49863ecf368e2a9d197b7e81
Tyler Johnson <tjohn73@uwo.ca>
2022-01-15 12:52:49 -0500

    Got the basic structure of the program out

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-
-# The main boi
+import json
+import sys
+import os
 from output import Output
+import duplicates
 
 
 if __name__ == "__main__":
@@ -8,5 +10,29 @@ if __name__ == "__main__":
     # Helps us with output
     out = Output()
 
+    files = sys.argv[1:]
+
+    for file in files:
+
+        try: 
     
+            data    = json.loads(file)
+            items   = data['items']
+            folders = data['folders']
+
+
+            # Clear up the folders and items
+            new_folders = duplicates.remove_duplicate_folders(folders, items)
+            new_items   = duplicates.remove_duplicate_items(items)
+
+            # Create the new file
+            open('new-' + file, 'w').close()
+
+            # Write out the new data
+            json.dump({'encrypted': False, 'folders': new_folders, 'items': new_items}, 'new_' + file) 
+
+        except:
+
+            out.fatal("idk this just died.")
+            os.exit(-1)
 
